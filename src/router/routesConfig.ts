00ef51e5d263e7630495ce2f5252cded89951a39
kele_Bingtang <2456019588@qq.com>
2024-06-22 22:14:27 +0800

    style: 🎨 添加路由配置的使用注释

--- src/router/routesConfig.ts
+++ src/router/routesConfig.ts
@@ -31,6 +31,7 @@
  * @param meta.dynamicLevel ==> 动态路由可打开的最大数量，默认为空
  * @param meta.useI18n ==>  是否开启 i18n。默认读取全局的 routeUseI18n（src/config/settings.ts）
  * @param meta.useTooltip ==> 菜单的文字超出后，是否使用 el-toolTip 提示，仅针二级路由及以上生效。默认读取全局的 routeUseTooltip（src/config/settings.ts）
+ * @param render ==> 自定义 Render 菜单元素（TSX 语法）
  */
 
 import componentRoutes from "./modules/components";
