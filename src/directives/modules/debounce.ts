fa22f984e67fc23583a30111c04489a672ad5551
kele_Bingtang <2456019588@qq.com>
2024-07-04 01:04:45 +0800

    feat: 🚀 防抖和节流指令支持指定的限制时间传入

--- src/directives/modules/debounce.ts
+++ src/directives/modules/debounce.ts
@@ -1,25 +1,34 @@
 /**
  * v-debounce
  * 按钮防抖指令，可自行扩展至input
- * 接收参数：function类型
+ * 接收参数：function 类型或者 {onClick: 接收参数：function, time: 2000} 类型
+ *
+ * <button v-debounce="debounceClick">防抖提交</button>
+ * <button v-debounce="{onClick: debounceClick, time: 1000}">防抖提交</button>
  */
 import type { Directive, DirectiveBinding } from "vue";
+
 interface ElType extends HTMLElement {
   __handleClick__: () => any;
 }
+
 const debounce: Directive = {
   mounted(el: ElType, binding: DirectiveBinding) {
-    if (typeof binding.value !== "function") {
+    const { value } = binding;
+    if (typeof value !== "function" && typeof value?.onClick !== "function") {
       throw Error("callback must be a function");
     }
+
+    const onClick = value.onClick || value;
+    const time = value.time || 500;
+
     let timer: NodeJS.Timeout | null = null;
     el.__handleClick__ = function () {
-      if (timer) {
-        clearInterval(timer);
-      }
+      if (timer) clearInterval(timer);
+
       timer = setTimeout(() => {
-        binding.value();
-      }, 500);
+        onClick();
+      }, time);
     };
     el.addEventListener("click", el.__handleClick__);
   },
--- src/directives/modules/throttle.ts
+++ src/directives/modules/throttle.ts
@@ -5,29 +5,37 @@
     1、第一次点击，立即调用方法并禁用按钮，等延迟结束再次激活按钮
     2、将需要触发的方法绑定在指令上
   使用：给 Dom 加上 v-throttle 及回调函数即可
-  <button v-throttle="debounceClick">节流提交</button>
+
+  <button v-throttle="throttleClick">节流提交</button>
+  <button v-throttle="{onClick: throttleClick, time: 2000}">节流提交</button>
 */
 import type { Directive, DirectiveBinding } from "vue";
+
 interface ElType extends HTMLElement {
   __handleClick__: () => any;
   disabled: boolean;
 }
+
 const throttle: Directive = {
   mounted(el: ElType, binding: DirectiveBinding) {
-    if (typeof binding.value !== "function") {
+    const { value } = binding;
+    if (typeof value !== "function" && typeof value?.onClick !== "function") {
       throw Error("callback must be a function");
     }
+
+    const onClick = value.onClick || value;
+    const time = value.time || 2000;
+
     let timer: NodeJS.Timeout | null = null;
     el.__handleClick__ = function () {
-      if (timer) {
-        clearTimeout(timer);
-      }
-      if (!el.disabled) {
-        el.disabled = true;
-        binding.value();
+      if (timer) clearTimeout(timer);
+
+      if (!el.classList.contains("is-disabled")) {
+        el.classList.add("is-disabled");
+        onClick();
         timer = setTimeout(() => {
-          el.disabled = false;
-        }, 1000);
+          el.classList.remove("is-disabled");
+        }, time);
       }
     };
     el.addEventListener("click", el.__handleClick__);
